f58426e1ccd5eb66f3c34043e49904ff3603052b
viv1990 <er.viveksoni90@gmail.com>
2026-07-26 17:06:31 +0530

    Added new Messages

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 Hello, this repo is created locally and will be pushed to Github.
 This change is done by feature 1 branch
+This change is done by feature 2 branch
--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def divide(a, b):
 
 a= int(input("Enter a number: "))
 b= int(input("Enter a number: "))
-print(add(a, b))
-print(subtract(a,b))
-print(multiply(a,b))
+print("The sum of the two numbers is: ",    add(a, b))
+print("The difference of the two numbers is: ", subtract(a,b))
+print("The product of the two numbers is: ", multiply(a,b))
 if b != 0:
-    print(divide(a,b))
+    print("The division of the two numbers is: ", divide(a,b))
 else:
     print("Error: Division by zero")
 
